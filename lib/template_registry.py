24babf5faf04d95fbd87120bfbbaae19d1dbd1d3
ukkeyHG <osaru3@saruyamax.com>
2018-02-28 19:27:31 +0900

    Update template_registry.py

--- lib/template_registry.py
+++ lib/template_registry.py
@@ -199,6 +199,11 @@ class TemplateRegistry(object):
         # Check if extranonce2 looks correctly. extranonce2 is in hex form...
         if len(extranonce2) != self.extranonce2_size * 2:
             raise SubmitException("Incorrect size of extranonce2. Expected %d chars" % (self.extranonce2_size*2))
+
+	# normalize the case to prevent duplication of valid shares by the client
+        ntime = ntime.lower()
+        nonce = nonce.lower()
+        extranonce2 = extranonce2.lower()
         
         # Check for job
         job = self.get_job(job_id)
